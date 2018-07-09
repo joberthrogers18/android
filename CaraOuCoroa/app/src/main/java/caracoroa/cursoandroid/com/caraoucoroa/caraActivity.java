8b00921de25a991e71949c511a3d88ff98d7f5af
Joberth Rogers <joberth.rogers18@gmail.com>
2018-07-08 22:52:40 -0300

    adding recue button action

--- CaraOuCoroa/app/src/main/java/caracoroa/cursoandroid/com/caraoucoroa/caraActivity.java
+++ CaraOuCoroa/app/src/main/java/caracoroa/cursoandroid/com/caraoucoroa/caraActivity.java
@@ -1,13 +1,27 @@
 package caracoroa.cursoandroid.com.caraoucoroa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class caraActivity extends AppCompatActivity {
+
+    private ImageView botaoVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cara);
+
+        botaoVoltar = (ImageView) findViewById(R.id.voltarCaraId);
+
+        botaoVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(caraActivity.this, MainActivity.class));
+            }
+        });
     }
 }
--- CaraOuCoroa/app/src/main/java/caracoroa/cursoandroid/com/caraoucoroa/coroaActivity.java
+++ CaraOuCoroa/app/src/main/java/caracoroa/cursoandroid/com/caraoucoroa/coroaActivity.java
@@ -1,5 +1,6 @@
 package caracoroa.cursoandroid.com.caraoucoroa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,15 @@ public class coroaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coroa);
+
+        botaoVoltar = (ImageView) findViewById(R.id.voltarCoroaId);
+
+        botaoVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(coroaActivity.this, MainActivity.class));
+            }
+        });
 
     }
 }
