a244f989cd8b11e2a52428ffa0f8c7eb283888a2
Joberth Rogers <joberth.rogers18@gmail.com>
2018-07-21 15:22:58 -0300

    Adding Listener
    When an object change the listenner update the data

--- FirebaseApp/app/src/main/java/com/cursoandroid/firebaseapp/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/com/cursoandroid/firebaseapp/firebaseapp/MainActivity.java
@@ -2,21 +2,25 @@ package com.cursoandroid.firebaseapp.firebaseapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference databaseReferencia = FirebaseDatabase.getInstance().getReference();
     private DatabaseReference usuarioReferencia = databaseReferencia.child("usuario"); //definindo filho para o projeto
-    private DatabaseReference produtoReferencia = databaseReferencia.child("produto");
+    private DatabaseReference produtoReferencia = databaseReferencia.child("produto").child("001");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /*
         Usuario usuario = new Usuario();
         usuario.setNome("Ana Helena");
         usuario.setSobrenome("Silva");
@@ -31,6 +35,19 @@ public class MainActivity extends AppCompatActivity {
 
         usuarioReferencia.child("001").setValue(usuario);
         produtoReferencia.child("001").setValue(produtos);
+        */
+
+        usuarioReferencia.addValueEventListener(new ValueEventListener() { // listened para caso haja alguma mudança no banco ele eh chamado
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) { // chamado sempre que os dados do DB foram mudados
+                Log.i("FIREBASE", dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { // caso haja erro na recuperação você pode chama-lo
+
+            }
+        });
 
 
     }
