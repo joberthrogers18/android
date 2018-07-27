8366ac52cff73ec6c02d8701d302734fee7bb4f6
Joberth Rogers <joberth.rogers18@gmail.com>
2018-07-27 00:25:56 -0300

    Set click on button and change the to fragment login

--- ProjetoFragmento/app/src/main/java/projetofragmento/whatsappandroid/cursoandroid/projetofragmento/LoginFragment.java
+++ ProjetoFragmento/app/src/main/java/projetofragmento/whatsappandroid/cursoandroid/projetofragmento/LoginFragment.java
@@ -6,12 +6,15 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
+
+    private TextView texto;
 
 
     public LoginFragment() {
@@ -23,7 +26,12 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false); // da pra fazer alterações no fragmete
+
+        texto = view.findViewById(R.id.tvLogin);
+        texto.setText("Tela de login alterada");
+
+        return view;
     }
 
 }
--- ProjetoFragmento/app/src/main/java/projetofragmento/whatsappandroid/cursoandroid/projetofragmento/MainActivity.java
+++ ProjetoFragmento/app/src/main/java/projetofragmento/whatsappandroid/cursoandroid/projetofragmento/MainActivity.java
@@ -1,13 +1,33 @@
 package projetofragmento.whatsappandroid.cursoandroid.projetofragmento;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoLogar = findViewById(R.id.btLogar);
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction(); //preparado para fazer as edições no fragmento
+
+                LoginFragment loginFragment = new LoginFragment();
+
+                fragmentTransaction.add(R.id.rl_container_fragment, loginFragment); // adicionar ao container nosso fragmento
+                fragmentTransaction.commit(); // confirmar a alteração
+            }
+        });
     }
 }
